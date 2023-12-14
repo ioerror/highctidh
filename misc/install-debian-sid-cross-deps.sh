059234d19e5971f88020197c6418510cd40d78a3
j <j@example.com>
2023-12-14 15:08:31 +0100

    CI: cross: try the generic gcc dists instead of specifically gcc-13

--- misc/install-debian-sid-cross-deps.sh
+++ misc/install-debian-sid-cross-deps.sh
@@ -4,9 +4,9 @@ set -e
 apt update
 apt install -y --no-install-recommends make gcc clang git python3 \
     python3-build python3-setuptools build-essential python3-venv \
-    python3-wheel python3-pip flit gcc-13 gcc-aarch64-linux-gnu \
-    binutils-aarch64-linux-gnu gcc-13-powerpc64-linux-gnu \
-    gcc-13-riscv64-linux-gnu gcc-13-s390x-linux-gnu \
+    python3-wheel python3-pip flit gcc gcc-aarch64-linux-gnu \
+    binutils-aarch64-linux-gnu gcc-powerpc64-linux-gnu \
+    gcc-riscv64-linux-gnu gcc-s390x-linux-gnu \
     gcc-mips64-linux-gnuabi64 binutils-mips64-linux-gnuabi64 \
     libc6-dev-arm64-cross libc6-dev-armel-cross libc6-dev-armhf-cross \
     libc6-dev-mips64-cross libc6-dev-mips64el-cross libc6-dev-ppc64-cross \
