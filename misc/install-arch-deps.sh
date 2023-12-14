0d3d4748aff7cc2435a7e419fc0b9c4a2b0a9a31
j <j@example.com>
2023-12-14 04:44:49 +0100

    build-alpine-3.18: now that we try to use clang to build things, we also need to install clang

--- misc/install-arch-deps.sh
+++ misc/install-arch-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apk update;
-apk add --no-cache alpine-sdk python3 python3-dev py3-build py3-flit py3-setuptools py3-wheel;
+apk add --no-cache alpine-sdk python3 python3-dev py3-build py3-flit py3-setuptools py3-wheel clang;
