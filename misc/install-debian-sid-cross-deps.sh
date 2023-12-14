8c2404da972dfe4caaa65f8e494156c4fd8f2f78
j <j@example.com>
2023-12-14 05:03:21 +0100

    debian-sid aarch64: gcc-13-aarch64-linux-gnu installs the compiler with the -13 suffix, this commit asks for the aliases package that installs version-independent symlinks

--- misc/install-debian-sid-cross-deps.sh
+++ misc/install-debian-sid-cross-deps.sh
@@ -4,7 +4,7 @@ set -e
 apt update
 apt install -y --no-install-recommends make gcc clang git python3 \
     python3-build python3-setuptools build-essential python3-venv \
-    python3-wheel python3-pip flit gcc-13 gcc-13-aarch64-linux-gnu \
+    python3-wheel python3-pip flit gcc-13 gcc-aarch64-linux-gnu \
     binutils-aarch64-linux-gnu gcc-13-powerpc64-linux-gnu \
     gcc-13-riscv64-linux-gnu gcc-13-s390x-linux-gnu \
     gcc-mips64-linux-gnuabi64 binutils-mips64-linux-gnuabi64
