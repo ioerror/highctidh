5aba28c047e7037361a0ab8b0623965f01c9e3e1
Jacob Appelbaum <jacob@appelbaum.net>
2023-12-19 14:28:16 +0100

    Update CI config
    This changeset updates the continuous integration configuration for the
    codeberg woodpecker CI. It also introduces a basic Github Action.
    
    - Passing but not currently enabled woodpecker configuration files are
      in `CI/*.yml`
    - C library: `.woodpecker/alpine.yml` tests if the project compiles for
      the most basic build
    - Golang: `.woodpecker/golang-test.yml` tests a native
      `amd64`/`x86_64` build of the C code as used in the golang bindings.
      It additionally runs a very basic golang test suite
    - Golang: `.woodpecker/golang.yml` runs if the `golang-test.yml` tests
      pass. It cross compiles golang `1.21`, `1.20`, and `1.19` for `amd64`,
      `arm32v5`, `arm32v6`, `arm32v7`, `i386`, `arm64`, `mips64`,
      `mips64le`, `mips`, `mipsle`, `ppc64`, `ppc64le`, `riscv64`, and
      `s390x` on Debian GNU/Linux `bookworm` with `clang`
    - Python: `.woodpecker/qemu-python-clang-setup.yml` uses `qemu` to
      compile native builds with Python `3.12`, `3.11`, `3.10`, and `3.9`
      for `amd64`, `arm32v5`, `arm32v6`, `arm32v7`, `i386`, `arm64`,
      `mips64le`, `ppc64le`, and `s390x` on Debian GNU/Linux `bookworm` with
      `clang`
    - Python: `.woodpecker/qemu-python-clang-test.yml` runs if
      `qemu-python-clang-setup.yml` completes successfully. It runs a
      versioned `pytest` for each Python version of `3.12`, `3.11`, `3.10`,
      and `3.9` for each highctidh field size of `511`, `512`, `1024`, and
      `2048` as native `pytest` tests for  `amd64`, `arm32v5`, `arm32v6`,
      `arm32v7`, `i386`, `arm64`, `mips64le`, `ppc64le`, and `s390x` on
      Debian GNU/Linux `bookworm`
    - Github Actions: `.github/workflows/makefile.yml` tests compiling the
      portable and the non-portable highctidh on the latest Ubuntu release

--- misc/install-debian-go-deps.sh
+++ misc/install-debian-go-deps.sh
@@ -2,7 +2,7 @@
 set -e
 
 dpkg --add-architecture i386;
-apt update > /dev/null 2>&1;
+apt update > /dev/null #2>&1;
 echo "Installing required packages...";
 apt install -y --no-install-recommends make gcc clang git build-essential \
     gcc-mipsel-linux-gnu gcc-mips64-linux-gnuabi64 gcc-mips64el-linux-gnuabi64 \
@@ -16,5 +16,5 @@ apt install -y --no-install-recommends make gcc clang git build-essential \
     libc6-dev-mipsel-cross libc6-dev-powerpc-ppc64-cross libc6-dev-ppc64-cross \
     libc6-dev-ppc64el-cross libc6-dev-riscv64-cross libc6-dev-s390x-cross \
     libc6-dev-sparc64-cross libc6-dev-sparc-sparc64-cross libc6-dev-i386 \
-    libc6-i386 libc6-dev linux-libc-dev linux-libc-dev:i386 > /dev/null 2>&1;
+    libc6-i386 libc6-dev linux-libc-dev linux-libc-dev:i386 # > /dev/null 2>&1;
 echo "Required packages installed";
