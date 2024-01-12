5b2134f378e74b79c8254af0f3bf52f26bd44d7a
Jacob Appelbaum <jacob@appelbaum.net>
2024-01-12 20:17:05 +0100

    Update the fiat-crypto synthesis process and docs
    - Update documentation
    - Update shell scripts
    - Update Dockerfile to pin versions

--- misc/docker-generate-fiat.sh
+++ misc/docker-generate-fiat.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
-set -e
+#!/bin/bash
+set -e;
 
-GID=`id -g`
+docker=$(type -p podman || type -p docker);
 
-docker run --mount type=bind,source="$(pwd)/docker_build_output/",target=/docker_build_output/ \
-   -u $UID:$GID \
-   --rm -it debian-libhighctidh-fiat-crypto:latest /highctidh/generate-fields.sh
+$docker run --mount type=bind,source="$(pwd)/docker_build_output/",target=/docker_build_output/ \
+   --rm -it debian-libhighctidh-fiat-crypto \
+   /highctidh/generate-fields.sh;
