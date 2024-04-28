a191e0fd74a6ec7533e73e3fa7e63c863c7756ff
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-28 22:02:55 +0200

    arch iter

--- misc/install-arch-deps.sh
+++ misc/install-arch-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e;
 
-apk update > /dev/null 2>&1;
+apk update #> /dev/null 2>&1;
 echo "Installing required packages...";
 apk add --no-cache alpine-sdk python3 python3-dev py3-build py3-flit \
-    py3-setuptools py3-wheel clang > /dev/null 2>&1;
+    py3-setuptools py3-wheel clang #> /dev/null 2>&1;
 echo "Required packages installed";
