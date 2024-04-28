3d7d92f99b186e884b4f69f11d0180fc1d518430
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-28 19:34:48 +0200

    fedora iter

--- misc/install-fedora-deps.sh
+++ misc/install-fedora-deps.sh
@@ -3,5 +3,5 @@ set -e;
 
 #dnf update #> /dev/null 2>&1;;
 echo "Installing required packages...";
-dnf install -y gcc make clang python3 python3-devel python3-flit python3-wheel python3-setuptools python3-build #> /dev/null 2>&1;
+dnf install -y gcc make clang python3 python3-devel python3-flit python3-wheel python3-setuptools python3-build sudo #> /dev/null 2>&1;
 echo "Required packages installed";
