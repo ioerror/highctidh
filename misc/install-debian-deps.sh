423e27e1f3c14c62fc7cff04386c731b0619b37e
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-28 19:52:05 +0200

    debian iter

--- misc/install-debian-deps.sh
+++ misc/install-debian-deps.sh
@@ -7,5 +7,5 @@ apt install -y --no-install-recommends make gcc clang git \
         python3 python3-build python3-setuptools python3-stdeb \
         build-essential python3-venv python3-wheel python3-pip \
         dh-python python3-all-dev flit fakeroot coreutils \
-        python3-pytest python3-pytest-xdist sudo > /dev/null 2>&1;
+        python3-pytest python3-pytest-xdist time sudo > /dev/null 2>&1;
 echo "Required packages installed";
