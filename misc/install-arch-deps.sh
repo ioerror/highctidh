089dc822b824b6b1f81b2a3812039f7eda2fa6a6
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-28 22:12:31 +0200

    arch iter

--- misc/install-arch-deps.sh
+++ misc/install-arch-deps.sh
@@ -3,6 +3,6 @@ set -e;
 
 pacman -Syu #> /dev/null 2>&1;
 echo "Installing required packages...";
-pacman -S gcc clang make python3 python3-dev py3-build py3-flit \
+pacman -yS gcc clang make python3 python3-dev py3-build py3-flit \
     py3-setuptools py3-wheel #> /dev/null 2>&1;
 echo "Required packages installed";
