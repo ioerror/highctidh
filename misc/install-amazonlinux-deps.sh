d036bec2ce69faec1347fd724ca9fc5ff0617236
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 16:19:39 +0200

    try oracle and amazon linux

---
+++ misc/install-amazonlinux-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e;
+
+echo "Installing required packages...";
+# XXX missing python3-flit python3-build
+# XXX some releases are also missing python3-wheel
+yum install -y gcc make clang python3 python3-devel python3-setuptools sudo #> /dev/null 2>&1;
+echo "Required packages installed";
