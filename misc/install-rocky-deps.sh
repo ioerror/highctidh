dbeb3783a842c809771b2a429070c9e667a911fe
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 14:47:49 +0200

    rocky iter

--- misc/install-rocky-deps.sh
+++ misc/install-rocky-deps.sh
@@ -2,5 +2,6 @@
 set -e;
 
 echo "Installing required packages...";
-dnf install -y gcc make clang python3 python3-devel python3-flit python3-wheel python3-setuptools python3-build sudo #> /dev/null 2>&1;
+# XXX missing python3-flit python3-build
+dnf install -y gcc make clang python3 python3-devel python3-wheel python3-setuptools sudo #> /dev/null 2>&1;
 echo "Required packages installed";
