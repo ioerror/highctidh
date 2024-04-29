c2ec5680acadb103aa48499aaaef1c2a05295b48
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 15:56:17 +0200

    clearlinux iter

--- misc/install-clearlinux-deps.sh
+++ misc/install-clearlinux-deps.sh
@@ -3,5 +3,5 @@ set -e;
 set -x;
 
 echo "Installing required packages...";
-swupd -y bundle-add -y sudo llvm make python3-basic # > /dev/null 2>&1;
+swupd bundle-add -y sudo llvm make python3-basic # > /dev/null 2>&1;
 echo "Required packages installed";
