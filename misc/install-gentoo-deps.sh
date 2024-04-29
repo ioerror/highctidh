48284e0191d7d78b904439f13eb9bd93e7fa2f77
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 15:17:30 +0200

    gentoo iter

--- misc/install-gentoo-deps.sh
+++ misc/install-gentoo-deps.sh
@@ -2,6 +2,7 @@
 set -e;
 
 echo "Installing required packages...";
+mkdir -p /var/db/repos/gentoo;
 emerge sys-devel/gcc #> /dev/null 2>&1;
 emerge app-admin/sudo #> /dev/null 2>&1;
 emerge dev-lang/python:3.11 #> /dev/null 2>&1;
