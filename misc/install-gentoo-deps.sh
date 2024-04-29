1192c32df341b2b09717d01f179dfc9606aebc63
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 15:24:42 +0200

    devuan iter

--- misc/install-gentoo-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e;
-
-echo "Installing required packages...";
-mkdir -p /var/db/repos/gentoo;
-emerge sys-devel/gcc #> /dev/null 2>&1;
-emerge app-admin/sudo #> /dev/null 2>&1;
-emerge dev-lang/python:3.11 #> /dev/null 2>&1;
-echo "Required packages installed";
