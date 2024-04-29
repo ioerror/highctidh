202ccd8a5d2df71d73ad93af7ada82eabe05d739
Jacob Appelbaum <jacob@appelbaum.net>
2024-04-29 14:31:18 +0200

    alma iter

--- misc/install-alma-deps.sh
+++ misc/install-alma-deps.sh
@@ -2,5 +2,6 @@
 set -e;
 
 echo "Installing required packages...";
-dnf install -y gcc make clang python3.11 python3.11-devel python3.11-flit python3.11-wheel python3-setuptools python3.11-build sudo #> /dev/null 2>&1;
+# XXX missing packages: python3-flit python3-build
+dnf install -y gcc make clang python3.11 python3.11-devel python3.11-wheel python3-setuptools sudo #> /dev/null 2>&1;
 echo "Required packages installed";
