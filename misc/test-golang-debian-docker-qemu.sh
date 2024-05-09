0758fbe4baf4815f57127eed987f868e15e98e56
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-09 21:27:27 +0200

    get ready

--- misc/test-golang-debian-docker-qemu.sh
+++ misc/test-golang-debian-docker-qemu.sh
@@ -3,17 +3,13 @@ set -e;
 set -x;
 
 echo "Updating Debian package sources";
-apt update # > /dev/null 2>&1;
+apt update > /dev/null 2>&1;
 echo "Installing required packages...";
 apt install -y --no-install-recommends make clang git \
-  golang build-essential fakeroot coreutils ca-certificates #> /dev/null 2>&1;
+  golang build-essential fakeroot coreutils ca-certificates > /dev/null 2>&1;
 echo "Required packages installed";
 echo "Configuring git";
 git config --global --add safe.directory .;
 echo "Running Golang tests";
-#export GOEXPERIMENT=cgocheck2;
-#export GODEBUG=cgocheck=1;
-#export CGO_LDFLAGS="-Wl,--no-as-needed -Wl,-allow-multiple-definition";
-export CC=clang
 go test -v ./...;
 echo "Golang tests completed";
