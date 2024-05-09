4d313dd1ddb190b8e1d9cdb1a62fdc5a91034c97
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-09 23:31:53 +0200

    shared directories are a problem but are they this problem? unlikely

--- misc/test-golang-alpine-docker-qemu.sh
+++ misc/test-golang-alpine-docker-qemu.sh
@@ -5,11 +5,12 @@ set -x;
 echo "Updating alpine package sources";
 apk update > /dev/null 2>&1;
 echo "Installing required packages...";
-apk add --no-cache alpine-sdk clang go > /dev/null 2>&1;
+apk add --no-cache alpine-sdk musl-dev clang go > /dev/null 2>&1;
 echo "Required packages installed";
 echo "Configuring git";
 git config --global --add safe.directory .;
 echo "Running Golang tests";
+export GOCACHE=`mktemp -d`
 go clean -v;
 go test -v ./...;
 echo "Golang tests completed";
