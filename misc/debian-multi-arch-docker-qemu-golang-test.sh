5ed203d3c47b953a6288bbf2751e06ef2eec6c81
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-09 21:19:12 +0200

    add packages

--- misc/debian-multi-arch-docker-qemu-golang-test.sh
+++ misc/debian-multi-arch-docker-qemu-golang-test.sh
@@ -5,11 +5,8 @@ set -x;
 echo "Updating Debian package sources";
 apt update # > /dev/null 2>&1;
 echo "Installing required packages...";
-apt install -y --no-install-recommends make gcc clang git \
-        python3 python3-build python3-setuptools python3-stdeb \
-        build-essential python3-venv python3-wheel python3-pip \
-        dh-python python3-all-dev flit fakeroot coreutils \
-        python3-pytest python3-pytest-xdist #> /dev/null 2>&1;
+apt install -y --no-install-recommends make clang git \
+  golang build-essential fakeroot coreutils #> /dev/null 2>&1;
 echo "Required packages installed";
 echo "Configuring git";
 git config --global --add safe.directory .;
