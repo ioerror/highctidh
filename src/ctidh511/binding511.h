3eb12cf8fa859de0a3641ad9f2919d80265b8fa8
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-09 20:27:47 +0200

    try to not have the symbol many times

--- src/ctidh511/binding511.h
+++ src/ctidh511/binding511.h
@@ -13,6 +13,8 @@ void highctidh_511_go_fillrandom(void *, void *, size_t);
 #define NAMESPACEBITS(x) highctidh_511_##x
 #define NAMESPACEGENERIC(x) highctidh_511_##x
 
+#if !defined(fillrandom_511_custom_FUNC)
+#define fillrandom_511_custom_FUNC
 __attribute__((weak))
 void fillrandom_511_custom(
   void *const outptr,
@@ -21,6 +23,7 @@ void fillrandom_511_custom(
 {
   highctidh_511_go_fillrandom((void *)context, outptr, outsz);
 }
+#endif
 #endif
 
 #endif
--- src/ctidh512/binding512.h
+++ src/ctidh512/binding512.h
@@ -13,6 +13,8 @@ void highctidh_512_go_fillrandom(void *, void *, size_t);
 #define NAMESPACEBITS(x) highctidh_512_##x
 #define NAMESPACEGENERIC(x) highctidh_512_##x
 
+#if !defined(fillrandom_512_custom_FUNC)
+#define fillrandom_512_custom_FUNC
 __attribute__((weak))
 void fillrandom_512_custom(
   void *const outptr,
@@ -21,6 +23,7 @@ void fillrandom_512_custom(
 {
   highctidh_512_go_fillrandom((void *)context, outptr, outsz);
 }
+#endif
 #endif
 
 #endif
