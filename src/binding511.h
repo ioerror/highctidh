45c6375870d1393878bfbeac38853f1516265f60
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-10 15:43:59 +0200

    iter

--- src/binding511.h
+++ src/binding511.h
@@ -19,7 +19,7 @@ void fillrandom_511_custom(
   const size_t outsz,
   const uintptr_t context)
 {
-  highctidh_2048_go_fillrandom((void *)context, outptr, outsz);
+  highctidh_511_go_fillrandom((void *)context, outptr, outsz);
 }
 #endif
 
