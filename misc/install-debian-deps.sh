9180ab80647e62a7c2a93d9ac45f9c1b443b4d8a
Jacob Appelbaum <jacob@appelbaum.net>
2024-06-06 13:58:13 +0200

    Add clang package back for CI

--- misc/install-debian-deps.sh
+++ misc/install-debian-deps.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Installing required packages...";
 apt update > /dev/null 2>&1;
-apt install -y --no-install-recommends make gcc clang-14 git \
+apt install -y --no-install-recommends make gcc clang-14 clang git \
         python3 python3-build python3-setuptools python3-stdeb \
         build-essential python3-venv python3-wheel python3-pip \
         dh-python python3-all-dev flit fakeroot coreutils \
