8381b4355ad32ac0877516edd7516e332b5dde56
Jacob Appelbaum <jacob@appelbaum.net>
2024-09-27 15:27:27 +0200

    add llvm to alpine deps

--- misc/install-alpine-deps.sh
+++ misc/install-alpine-deps.sh
@@ -4,5 +4,5 @@ set -e;
 apk update #> /dev/null 2>&1;
 echo "Installing required packages...";
 apk add --no-cache alpine-sdk bash python3 python3-dev py3-build py3-flit \
-    py3-setuptools py3-wheel py3-pip clang compiler-rt go py3-pytest py3-pytest-xdist > /dev/null 2>&1;
+    py3-setuptools py3-wheel py3-pip clang llvm compiler-rt go py3-pytest py3-pytest-xdist > /dev/null 2>&1;
 echo "Required packages installed";
