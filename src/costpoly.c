d3b3507c0b5a57e35cd22c3dd82e4ce51d3899fa
p <p@example.com>
2024-12-02 20:08:32 +0000

    remove fp_mulsq_count, fp_sq_count, fp_addsub_count and tunemults.c
    This commit dedicates the commited works to the public domain.

--- src/costpoly.c
+++ src/costpoly.c
@@ -10,27 +10,21 @@ int main()
     fp P[2*n];
     long long s = poly_tree1size(n);
     fp T[s];
-    fp_mulsq_count = 0;
     poly_tree1(T,P,n);
-    printf(", %lld",fp_mulsq_count);
   }
   printf(")\n");
 
   printf("multiprod2 = (0");
   for (long long n = 1;n <= 64;++n) {
     fp T[3*n];
-    fp_mulsq_count = 0;
     poly_multiprod2(T,n);
-    printf(", %lld",fp_mulsq_count);
   }
   printf(")\n");
 
   printf("multiprod2_selfreciprocal = (0");
   for (long long n = 1;n <= 64;++n) {
     fp T[3*n];
-    fp_mulsq_count = 0;
     poly_multiprod2_selfreciprocal(T,n);
-    printf(", %lld",fp_mulsq_count);
   }
   printf(")\n");
 
@@ -50,13 +44,11 @@ int main()
         fp f[flen];
         long long t = poly_multieval_precomputesize(n,flen);
         fp precomp[t];
-        fp_mulsq_count = 0;
         poly_multieval_precompute(precomp,n,flen,P,T);
         if (post) {
-          fp_mulsq_count = 0;
           poly_multieval_postcompute(v,n,f,flen,P,T,precomp);
         }
-        printf("%lld:%lld,",flen,fp_mulsq_count);
+        printf("%lld,",flen);
       }
       printf("},\n");
     }
