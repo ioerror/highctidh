c5ca7c83caebaf02c9f791a3e2f0f1abe0d18ef0
Jacob Appelbaum <jacob@appelbaum.net>
2024-12-17 17:01:54 +0100

    remove python-devtools

--- misc/install-arch-deps.sh
+++ misc/install-arch-deps.sh
@@ -2,6 +2,6 @@
 set -ex;
 
 echo "Installing required packages...";
-pacman -Syu --noconfirm gcc clang make sudo python3 python-devtools python-build python-flit \
+pacman -Syu --noconfirm gcc clang make sudo python3 python-build python-flit \
     python-setuptools python-wheel #> /dev/null 2>&1;
 echo "Required packages installed";
