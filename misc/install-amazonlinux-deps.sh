ce849ffdb2bd963f20d8268b4a2cdf8d45b302dc
p <p@example.com>
2025-02-10 02:47:46 +0100

    CI: simpler docker-linux.yaml and install-deps.sh

--- misc/install-amazonlinux-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e;
-
-echo "Installing required packages...";
-# XXX missing python3-flit python3-build
-# XXX some releases are also missing python3-wheel
-yum install -y gcc make clang python3 python3-devel python3-setuptools sudo tar gzip git time autoconf automake libtool > /dev/null 2>&1
-echo "Required packages installed";
